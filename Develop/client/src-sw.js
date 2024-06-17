334ccbd9e0c2acc52b3d872cb5c9b2ca2e5a9900
Patrick Peterson <ppeterson609@gmail.com>
2024-06-17 15:48:47 -0400

    added all routes & merged server

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,12 +1,10 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+const { precacheAndRoute } = require('workbox-precaching');
 precacheAndRoute(self.__WB_MANIFEST);
-
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,7 +12,7 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, 
     }),
   ],
 });
@@ -25,6 +23,35 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(
+  ({ request }) => request.destination === 'style' || request.destination === 'script',
+  new StaleWhileRevalidate({
+    cacheName: 'static-resources',
+    plugins: [
+      new CacheableResponsePlugin({ statuses: [0, 200] }),
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 365 * 24 * 60 * 60, 
+      }),
+    ],
+  })
+);
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({ statuses: [0, 200] }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, 
+      }),
+    ],
+  })
+);
 
-// TODO: Implement asset caching
-registerRoute();
+// Offline fallbacks
+offlineFallback({
+  pageFallback: '/index.html', 
+  imageFallback: '/images/logo.png',
+});
